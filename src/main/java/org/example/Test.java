3206fb7de53dbd6f4759f593f4dbb95c7a7c5ac9
alex <alex06072023@gmail.com>
2025-07-04 09:19:03 +0100

    for testing

---
+++ src/main/java/org/example/Test.java
@@ -0,0 +1,10 @@
+package org.example;
+
+public class Test {
+
+    public static void main(String[] args) {
+        System.out.println("*****************************");
+        System.out.println("Hello, Alex, good luck");
+        System.out.println("*****************************");
+    }
+}
